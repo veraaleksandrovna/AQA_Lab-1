6ccb306ce70e2a9717af19dbfe5b55a6f0d25287
Vera <veraaleksandrovna@mail.ru>
2022-02-23 15:38:35 +0300

    T3: Readonly to const

--- Task(new)/Constants/Constants.cs
+++ Task(new)/Constants/Constants.cs
@@ -2,21 +2,21 @@ namespace DriverAndCars;
 
 public class Constants
 {
-    public static readonly string language = "en";
+    public const string language = "en";
 
-    public static readonly int ammount = 3;
+    public const int ammount = 3;
     
-    public static readonly int minYear = 1999;
-    public static readonly int maxYear = 2021;
+    public const int minYear = 1999;
+    public const int maxYear = 2021;
 
-    public static readonly double minCapacity = 1.5D;
-    public static readonly double maxCapacity = 4.0D;
+    public const double minCapacity = 1.5D;
+    public const double maxCapacity = 4.0D;
 
-    public static readonly int minPower = 150;
-    public static readonly int maxPower = 300;
+    public const int minPower = 150;
+    public const int maxPower = 300;
 
-    public static readonly int minSpeed = 200;
-    public static readonly int maxSpeed = 300;
+    public const int minSpeed = 200;
+    public const int maxSpeed = 300;
 
-    public static readonly int minAge = 16;
+    public const int minAge = 16;
 }
