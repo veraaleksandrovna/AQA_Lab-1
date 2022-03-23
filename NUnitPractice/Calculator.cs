cb5ee2ed7409a51b94785bbd2a548d5dcd8a893c
veraaleksandrovna <70635899+veraaleksandrovna@users.noreply.github.com>
2022-03-23 13:48:40 +0300

    T12: Add NUnit project

---
+++ NUnitPractice/Calculator.cs
@@ -0,0 +1,21 @@
+﻿namespace TestProject1
+{
+
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
