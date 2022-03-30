7ddb39bfa9e0a1d464ed8363ef2c221c033d641d
veraaleksandrovna <70635899+veraaleksandrovna@users.noreply.github.com>
2022-03-31 02:03:40 +0300

    T14: Output elements in console

--- Locators/Locators/LocatorsTests.cs
+++ Locators/Locators/LocatorsTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -25,6 +26,11 @@ public class LocatorsTests
        Assert.IsTrue(_driver.FindElement(By.CssSelector("h1>span")).Displayed);
        Assert.IsTrue(_driver.FindElement(By.CssSelector("span[value^='12']")).Displayed);
 
+       Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("span[id*='123']")).Text}");
+       Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector(".arrow")).Text}");
+       Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("span[id='123']")).Text}");
+       Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("h1>span")).Text}");
+       Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("span[value^='12']")).Text}");
     }
     
     [Test]
@@ -35,6 +41,13 @@ public class LocatorsTests
         Assert.IsTrue(_driver.FindElement(By.XPath("//*[contains(text(),'Title 2')]")).Displayed);
         Assert.IsTrue(_driver.FindElement(By.XPath("//h1/span[contains(text(),'Title 3')]")).Displayed);
         Assert.IsTrue(_driver.FindElement(By.XPath("(//div[contains(.//span, 'Title 2')]/descendant::span[@class='arrow'])[2]")).Displayed);
+        
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("(//*[contains(text(),'Test')])[2]")).Text}");
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("//*[contains(text(),'Test')][@ids]")).Text}");
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("//*[contains(text(),'Title 2')]")).Text}");
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("//h1/span[contains(text(),'Title 3')]")).Text}");
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("(//div[contains(.//span, 'Title 2')]/descendant::span[@class='arrow'])[2]")).Text}");
+        
     }
 
     [OneTimeTearDown]
