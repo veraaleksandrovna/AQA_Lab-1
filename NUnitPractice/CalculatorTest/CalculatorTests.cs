588b6763f6d44321fe639c49aaacf023c0777d5a
veraaleksandrovna <70635899+veraaleksandrovna@users.noreply.github.com>
2022-03-30 18:43:49 +0300

    T12: Change Test naming

--- NUnitPractice/CalculatorTest/CalculatorTests.cs
+++ NUnitPractice/CalculatorTest/CalculatorTests.cs
@@ -31,7 +31,7 @@ public class Tests
     [Test]
     [Category("SmokeTest")]
     [TestCaseSource(typeof(CalculatorData), nameof(CalculatorData.DivideIntegerTestData))]
-    public void DivideIntegerTest(int firstNumber, int secondNumber, int expectedResult)
+    public void DivideIntegerMethodTest(int firstNumber, int secondNumber, int expectedResult)
     {
         TestContext.Out.WriteLineAsync("First number is " + firstNumber);
         TestContext.Out.WriteLineAsync("Second number is" + secondNumber);
@@ -45,7 +45,7 @@ public class Tests
     [TestCase(int.MaxValue, 0)]
     [TestCase(int.MinValue, 0)]
     [TestCase(0, 0)]
-    public void DivideByZeroIntegerTest(int firstNumber, int secondNumber)
+    public void DivideInteger_DivideByZero_ThrowException(int firstNumber, int secondNumber)
     {
         Assert.Throws<DivideByZeroException>(delegate { _calculator.Div(firstNumber, secondNumber); });
     }
@@ -53,7 +53,7 @@ public class Tests
     [Test]
     [Category("SmokeTest")]
     [TestCaseSource(typeof(CalculatorData), nameof(CalculatorData.DivideDoubleTestData))]
-    public void DivideDoubleTest(double firstNumber, double secondNumber, double expectedResult)
+    public void DivideDoubleMethodTest(double firstNumber, double secondNumber, double expectedResult)
     {
         TestContext.Out.WriteLineAsync("First number is " + firstNumber);
         TestContext.Out.WriteLineAsync("Second number is" + secondNumber);
@@ -64,7 +64,7 @@ public class Tests
 
     [Test]
     [Category("SmokeTest")]
-    public void DivideByZeroDoubleTest()
+    public void DivideDouble_DivideByZero_True()
     {
         Assert.True(Double.IsPositiveInfinity(_calculator.Div(Double.MaxValue, 0d)));
         Assert.IsTrue(Double.IsNegativeInfinity(_calculator.Div(Double.MinValue, 0d)));
