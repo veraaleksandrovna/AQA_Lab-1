7d649bee76590dc2cca82459c5ce9dfd0f7bcc36
veraaleksandrovna <dropxx24@mail.ru>
2022-04-01 23:33:48 +0300

    T14: Edit asserts and some xpath

--- Locators/Locators/LocatorsTests.cs
+++ Locators/Locators/LocatorsTests.cs
@@ -19,34 +19,42 @@ public class LocatorsTests
     [Test]
     public void CssLocatorsTest()
     {
-       Assert.IsTrue(_driver.FindElement(By.CssSelector("span[id*='123']")).Displayed);
-       Assert.IsTrue(_driver.FindElement(By.CssSelector(".arrow")).Displayed);
-       Assert.IsTrue(_driver.FindElement(By.CssSelector("span[id='123']")).Displayed);
-       Assert.IsTrue(_driver.FindElement(By.CssSelector("h1>span")).Displayed);
-       Assert.IsTrue(_driver.FindElement(By.CssSelector("span[value^='12']")).Displayed);
+        Assert.Multiple(() =>
+            {
+                Assert.IsTrue(_driver.FindElement(By.CssSelector("span[id*='123']")).Displayed);
+                Assert.IsTrue(_driver.FindElement(By.CssSelector(".arrow")).Displayed);
+                Assert.IsTrue(_driver.FindElement(By.CssSelector("span[id='123']")).Displayed);
+                Assert.IsTrue(_driver.FindElement(By.CssSelector("h1>span")).Displayed);
+                Assert.IsTrue(_driver.FindElement(By.CssSelector("span[value^='12']")).Displayed);
+            });
 
-       Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("span[id*='123']")).Text}");
-       Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector(".arrow")).Text}");
-       Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("span[id='123']")).Text}");
-       Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("h1>span")).Text}");
-       Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("span[value^='12']")).Text}");
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("span[id*='123']")).Text}");
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector(".arrow")).Text}");
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("[id='123']")).Text}");
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("h1>span")).Text}");
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.CssSelector("span[value^='12']")).Text}");
     }
-    
+
     [Test]
     public void XPathTest()
     {
-        Assert.IsTrue(_driver.FindElement(By.XPath("(//*[contains(text(),'Test')])[2]")).Displayed);
-        Assert.IsTrue(_driver.FindElement(By.XPath("//*[contains(text(),'Test')][@ids]")).Displayed);
-        Assert.IsTrue(_driver.FindElement(By.XPath("//*[contains(text(),'Title 2')]")).Displayed);
-        Assert.IsTrue(_driver.FindElement(By.XPath("//h1/span[contains(text(),'Title 3')]")).Displayed);
-        Assert.IsTrue(_driver.FindElement(By.XPath("(//div[contains(.//span, 'Title 2')]/descendant::span[@class='arrow'])[2]")).Displayed);
-        
+        Assert.Multiple(() =>
+        {
+            Assert.IsTrue(_driver.FindElement(By.XPath("(//*[contains(text(),'Test')])[2]")).Displayed);
+            Assert.IsTrue(_driver.FindElement(By.XPath("//*[text() ='Test'][@ids]")).Displayed);
+            Assert.IsTrue(_driver.FindElement(By.XPath("//*[contains(text(),'Title 2')]")).Displayed);
+            Assert.IsTrue(_driver.FindElement(By.XPath("//*[normalize-space() ='Title 3']/ancestor::h1")).Displayed);
+            Assert.IsTrue(_driver
+                .FindElement(By.XPath("(//div[contains(.//span, 'Title 2')]/descendant::span[@class='arrow'])[2]"))
+                .Displayed);
+        });
+
         Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("(//*[contains(text(),'Test')])[2]")).Text}");
-        Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("//*[contains(text(),'Test')][@ids]")).Text}");
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("//*[text() ='Test'][@ids]")).Text}");
         Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("//*[contains(text(),'Title 2')]")).Text}");
-        Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("//h1/span[contains(text(),'Title 3')]")).Text}");
-        Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("(//div[contains(.//span, 'Title 2')]/descendant::span[@class='arrow'])[2]")).Text}");
-        
+        Console.Out.WriteLineAsync($"{_driver.FindElement(By.XPath("//*[normalize-space() ='Title 3']/ancestor::h1")).Text}");
+        Console.Out.WriteLineAsync(
+            $"{_driver.FindElement(By.XPath("(//div[contains(.//span, 'Title 2')]/descendant::span[@class='arrow'])[2]")).Text}");
     }
 
     [OneTimeTearDown]
