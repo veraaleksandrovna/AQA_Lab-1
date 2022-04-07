7896028bd07e892e7019ff2c38892e8225569dbd
veraaleksandrovna <dropxx24@mail.ru>
2022-04-08 00:29:25 +0300

    T16: Add solution

---
+++ AlertsWaits/AlertsWaits/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace AlertsWaits;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
