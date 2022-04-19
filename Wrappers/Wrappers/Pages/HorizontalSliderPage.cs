5f5b5d735d4e0222b3d9b4426883e702119bab9e
veraaleksandrovna <dropxx24@mail.ru>
2022-04-19 22:43:36 +0300

    T17:Edit configurations

---
+++ Wrappers/Wrappers/Pages/HorizontalSliderPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+
+namespace Wrappers.Pages;
+
+public class HorizontalSliderPage: BasePage
+{
+    private const string EndPoint = "/horizontal_slider";
+    
+    public HorizontalSliderPage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+    {
+    }
+
+    protected override void OpenPage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override bool IsPageOpened()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Wrappers/Wrappers/Pages/TablePage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+
+namespace Wrappers.Pages;
+
+public class TablePage:BasePage
+{
+    private const string EndPoint = "/challenging_dom";
+    
+    public TablePage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+    {
+    }
+
+    protected override void OpenPage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override bool IsPageOpened()
+    {
+        throw new System.NotImplementedException();
+    }
+}
